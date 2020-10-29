167b47c9516fc5a1ef9490e248f9b1559c82146e
kelbri10 <kelbri10@gmail.com>
2020-10-29 18:07:34 -0400

    updated routes to get, post, and put api requests to update desserts that user devours and adds

--- controllers/sweets_controller.js
+++ controllers/sweets_controller.js
@@ -1,29 +1,38 @@
 const express = require('express'); 
-const sweets = require('../models/sweets'); 
+const sweet = require('../models/sweets'); 
 const router = express.Router(); 
 
 //create the router for the app
 
 //get request
 router.get('/', (req, res) => { 
-    res.redirect('/sweets'); 
+    sweet.all( data => { 
+        let hbsObject = { 
+            sweets: data 
+        }; 
+
+        console.log(hbsObject); 
+        res.render('index', hbsObject); 
+    })
 }); 
 
-router.get('/sweets', (req,res)=>{ 
-    sweets.selectAll(function(sweets) { 
-        res.render('index', {sweets: sweets}); 
-    }); 
-})
-
 //post request 
-router.get('/sweets/create', (req,res) => { 
-    sweets.insertOne('/'); 
+router.post('/api/sweets', (req, res) => { 
+    sweet.create(['dessert', 'devoured'], [req.body.dessert, req.body.devoured], (result) => { 
+        res.json({ id: result.insertId }); 
+    })
 })
 
 //put request by id
-router.put('/sweets/:id', (req,res) => { 
-    sweets.updateOne(req.params.id, function (result) { 
-        res.sendStatus(200); 
+router.put('/api/sweets/:id', (req,res) => { 
+    let condition = `id = ${req.params.id}`;  
+
+    sweet.update({ devoured: req.body.devoured }, condition, (result) => { 
+        if (result.changedRows == 0) { 
+            return res.status(404).end(); 
+        } else { 
+            res.status(200).end(); 
+        }
     }); 
 }); 
 
