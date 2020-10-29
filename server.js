298aa79fe18610da6a16b1a65e6c286b9499a3cd
kelbri10 <kelbri10@gmail.com>
2020-10-29 18:30:03 -0400

    updated index.hbs with sections for devoured and waiting to be eaten, added partials folder to seperate dessert-blocks

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 //middleware 
 app.use(express.static('/public')); 
 app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true })); 
 
 //set handlebars 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'})); 
