9a02d97a51414d524cf1a281d00347695a24404f
kelbri10 <kelbri10@gmail.com>
2020-10-29 17:56:55 -0400

    added helper functions to orm for SQL syntax, updated methods in orm object

--- config/orm.js
+++ config/orm.js
@@ -2,13 +2,43 @@ const connection = require('connection.js');
 const { connect } = require('./connection');
 const consoleTable = require('console.table'); 
 
-//methods to retrive and store data in my database 
+//helper function to generate question marks 
+const printQuestionMarks = num => { 
+    let arr = []; 
+
+    for (let i = 0; i < num; i++) { 
+        arr.push('?'); 
+    }
+
+    return arr.toString(); 
+}
+
+//helper function to convert object key/value pairs to SQL syntax 
+const objToSql = ob => { 
+    let arr = []; 
+
+    for (let key in ob) { 
+        let value = ob[key]; 
+
+        if (Object.hasOwnProperty.call(ob, key)) { 
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'"; 
+            }
+
+            arr.push(key + '=' + value); 
+        }
+    }
+
+    return arr.toString(); 
+}
+
+//ORM object for all SQL statements 
 const orm = { 
 
     all: (tableInput, cb) => { 
-        let query = `SELECT * FROM ??`; 
+        let query = `SELECT * FROM ${tableInput}`; 
 
-        connection.query(query, [tableInput], (err, result) => { 
+        connection.query(query, (err, result) => { 
             if (err) throw err; 
 
             cb(result); 
@@ -16,20 +46,18 @@ const orm = {
     },
 
     create: (table, cols, vals, cb) => { 
-        let query  = `INSERT INTO ()
-                    VALUES ()`
+        let query  = 'INSERT INTO ' + table + ' (' + cols.toString() + ') VALUES (' + printQuestionMarks(vals.length) + ') '; 
         
-        connection.query(query, (err, result) => {
+        connection.query(query, vals, (err, result) => {
             if (err) throw err; 
 
             cb(result); 
         })
     }, 
 
-    update: (table, obj, condition, cb) => {
-        let query = `UPDATE 
-                    SET 
-                    WHERE `
+    update: (table, objColVals, condition, cb) => {
+        let query = 'UPDATE ' + table + ' SET ' + objToSql(objColVals) + ' WHERE ' + condition; 
+        
         connection.query(query, (err, result) => {
             if(err) throw err; 
 
